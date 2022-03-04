860d034df1dcad55b856fe44413ce5b66d8bc1b5
Reina <rbuen34@gmail.com>
2022-03-04 12:52:28 -0800

    player cannon ball ability + other
    player changes color when it gets hurt

--- Sky Pirates/Assets/Scripts/PlayerController.cs
+++ Sky Pirates/Assets/Scripts/PlayerController.cs
@@ -11,13 +11,19 @@ public class PlayerController : MonoBehaviour
     // game objects
     public Rigidbody2D myRb;
     public GameObject bullet;
+    public GameObject cannonBall;
 
     // lifespans and Timers
     public float bulletLifespan = 2.5f;
+    public float ballCooldown = 5;
 
     // healths
     public int health = 100;
 
+    // bools
+    public bool ballin = true;
+
+    // sprites
     public Sprite playerUp;
     public Sprite playerDown;
     public Sprite playerNeut;
@@ -51,6 +57,17 @@ public class PlayerController : MonoBehaviour
             Destroy(b, bulletLifespan);
         }
 
+        // ABILITY CODE
+
+        if (Input.GetKeyDown(KeyCode.Mouse1) & ballin)
+        {
+            GameObject c = Instantiate(cannonBall, transform);
+            Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), c.GetComponent<CircleCollider2D>());
+            c.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed, 0);
+            StartCoroutine("cannonCooldown");
+            Destroy(c, bulletLifespan);
+        }
+
 
         // HEALTH CODE
 
@@ -73,13 +90,37 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+
+
+    // timers
+
+    IEnumerator cannonCooldown ()
+    {
+        ballin = false;
+        Debug.Log("Wait for it");
+        yield return new WaitForSeconds(ballCooldown);
+        ballin = true;
+        Debug.Log("Done");
+    }
+
+    IEnumerator ow ()
+    {
+        // change the player color if it takes damage
+        GetComponent<SpriteRenderer>().color = new Vector4(0.8962264f, 0.3255162f, 0.3255162f, 1f);
+        yield return new WaitForSeconds(1);
+        GetComponent<SpriteRenderer>().color = new Vector4(1f, 1f, 1f, 1f);
+    }
+
+
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "eBullet")
         {
+            StartCoroutine("ow");
             Debug.Log("ouchie");
             health = health - 5;
-            // health--;
             Destroy(collision.gameObject);
         }
     }
