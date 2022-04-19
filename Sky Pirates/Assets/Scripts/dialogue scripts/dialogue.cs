a5c84417d7c8497bc62b3286808bc40915bf92ed
Reina <rbuen34@gmail.com>
2022-04-19 12:27:44 -0700

    dialogue works even better now
    yeaaaaah let's go!!!! they can talk to each other now

--- Sky Pirates/Assets/Scripts/dialogue scripts/dialogue.cs
+++ Sky Pirates/Assets/Scripts/dialogue scripts/dialogue.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class dialogue
 {
-    public string name;
+    public string[] names;
+    public Sprite[] talksprites;
 
     [TextArea(3, 10)]
     
--- Sky Pirates/Assets/Scripts/dialogue scripts/dialogueManager.cs
+++ Sky Pirates/Assets/Scripts/dialogue scripts/dialogueManager.cs
@@ -7,28 +7,51 @@ public class dialogueManager : MonoBehaviour
 {
 
     private Queue<string> sentences;
+    private Queue<string> names;
+    private Queue<Sprite> tSprites;
     public dialogueTrigger dialogueTrigger;
 
     public Text nameText;
     public Text dialogueText;
+    public Image spriteImage;
 
     // Start is called before the first frame update
     void Start()
     {
+        // new sentence
         sentences = new Queue<string>();
+
+        // new name?
+        names = new Queue<string>();
+
+        // new talksprites
+        tSprites = new Queue<Sprite>();
+        spriteImage = spriteImage.GetComponent<Image>();
     }
 
     public void StartDialogue (dialogue dialogue)
     {
-        Debug.Log("Starting conversation with" + dialogue.name);
+        // Debug.Log("Starting conversation with" + dialogue.name);
 
-        nameText.text = dialogue.name;
-
+        // clear existing sentences
         sentences.Clear();
+        names.Clear();
+        tSprites.Clear();
 
+        // display each sentence
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
+        }
+
+        foreach (string name in dialogue.names)
+        {
+            names.Enqueue(name);
+        }
+
+        foreach (Sprite sprite in dialogue.talksprites)
+        {
+            tSprites.Enqueue(sprite);
         }
 
         DisplayNextSentence();
@@ -36,17 +59,23 @@ public class dialogueManager : MonoBehaviour
 
     public void DisplayNextSentence()
     {
+        // if no more sentences then end convo
         if (sentences.Count == 0)
         {
             EndDialogue();
             return;
         }
 
+        Sprite sprite = tSprites.Dequeue();
+        string name = names.Dequeue();
         string sentence = sentences.Dequeue();
         StopAllCoroutines();
+        nameText.text = name;
+        spriteImage.sprite = sprite;
         StartCoroutine(TypeSentence(sentence));
     }
 
+    // type the sentence out
     IEnumerator TypeSentence (string sentence)
     {
         dialogueText.text = "";
@@ -59,7 +88,7 @@ public class dialogueManager : MonoBehaviour
 
     public void EndDialogue()
     {
-        Debug.Log("end of convo");
+        // Debug.Log("end of convo");
         dialogueTrigger.inConvo = false;
     }
 }
